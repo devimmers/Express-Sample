9b83f233bd088ecb1a81f1a33f882502b1f33114
Sergey Rudenko <rudenko.sergey92@gmail.com>
2013-03-26 09:06:20 +0200

    Refactor models to module

--- routes/items.js
+++ routes/items.js
@@ -1,28 +1,26 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    Item = require('../models/item.js').model;
 
 mongoose.connect('mongodb://localhost/itemsdb');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-var itemSchema = mongoose.Schema({
-    name: String,
-    year: String,
-    price: Number,
-    description: String,
-    picture: String
-})
-
-var Item = mongoose.model('Item', itemSchema)
-
+//Create test Item
 var exampleItem = new Item({
     name: "qweqwe",
     year: "2022",
     price: 55,
     description: "asd",
     picture: "asdasd"
-})
+});
 
+//Find all items
+exports.findAll = function() {
+    Item.find({}, function(error, data){
+        console.log(data);
+    });
+};
 
 exampleItem.save(function (err, data) {
     if (err) // TODO handle the error
