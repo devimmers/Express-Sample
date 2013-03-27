3ea1a20da0295841e2c326d5055982276c674c6f
Sergey Rudenko <rudenko.sergey92@gmail.com>
2013-03-27 23:35:42 +0200

    Add base standat init

--- models/item.js
+++ models/item.js
@@ -7,8 +7,25 @@ var itemSchema = new mongoose.Schema({
     description: String,
     picture: String
 });
+var Item = mongoose.model('Item', itemSchema);
 
-exports.model = mongoose.model('Item', itemSchema);
+var test_item = new Item({name:"qwe", year:"123", price:213, description:"qwe", picture:"qweqwe"});
+
+function baseInit() {
+    for(var i=0; i<10; i++) {
+        test_item.save(function(err){
+            if (err) {
+                console.log("Eroro saved");
+                res.send("Fail");
+                throw err;
+            }
+            console.log("Saved");
+        });
+    }
+}
+
+baseInit();
+exports.model = Item;
 exports.schema = itemSchema;
 
 
